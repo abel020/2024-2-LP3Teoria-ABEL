90ec051b8bbedfa6be879e842f50472dc54dea4d
starkvicco <zzzsafo@gmail.com>
2024-09-30 15:28:21 -0500

    Eliminar archivos

---
+++ Semana06/Sobrescribir_Archivo.py
@@ -0,0 +1,5 @@
+archivo = open("noticia.txt","at")
+contenido = "\nFuente: RPP"
+archivo.write(contenido)
+
+archivo.close()
---
+++ Semana06/grabar_archivo.py
@@ -0,0 +1,6 @@
+archivo = open("prueba.txt","wt")
+contenido = "saludos cordiales\nAtentamente\nAbel"
+
+archivo.write(contenido)
+
+archivo.close()
